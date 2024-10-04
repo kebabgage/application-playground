1e21662803d1ba89c63445e654883b865aa1c0f7
Kaleb Bartlett-Elliott <kaleb.bartlett-elliott@transmax.com.au>
2024-10-04 17:00:18 +0200

    Updating aws-deploy.sh

--- dotnet/ApplicationPlayground/aws-deploy.sh
+++ dotnet/ApplicationPlayground/aws-deploy.sh
@@ -2,5 +2,5 @@ sudo docker pull 443370684269.dkr.ecr.ap-southeast-2.amazonaws.com/recipes/dotne
 sudo docker stop dotnet
 sudo docker rm dotnet
 cd ../../..
-sudo docker run -d -p 8000:8080 --volume ./filesystem:/files --network my-network --name dotnet 443370684269.dkr.ecr.ap-southeast-2.amazonaws.com/recipes/dotnet:latest
+sudo docker run -d -e OPEN_AI_KEY=$OPEN_AI_KEY -p 8000:8080 --volume ./filesystem:/files --network my-network --name dotnet 443370684269.dkr.ecr.ap-southeast-2.amazonaws.com/recipes/dotnet:latest
 cd ./application-playground/dotnet/ApplicationPlayground
