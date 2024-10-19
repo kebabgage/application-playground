4090a5e677a7a204b2ec95223f5817a13dcc587a
Kaleb Bartlett-Elliott <kaleb.bartlett-elliott@transmax.com.au>
2024-10-19 15:11:54 +0200

    Updating aws-deploy script for debubbin

--- dotnet/ApplicationPlayground/aws-deploy.sh
+++ dotnet/ApplicationPlayground/aws-deploy.sh
@@ -2,5 +2,6 @@ sudo docker pull 443370684269.dkr.ecr.ap-southeast-2.amazonaws.com/recipes/dotne
 sudo docker stop dotnet
 sudo docker rm dotnet
 cd ../../..
+echo $OPEN_AI_KEY
 sudo docker run -d -e OPEN_AI_KEY=$OPEN_AI_KEY -p 8000:8080 --volume ./filesystem:/files --network my-network --name dotnet 443370684269.dkr.ecr.ap-southeast-2.amazonaws.com/recipes/dotnet:latest
 cd ./application-playground/dotnet/ApplicationPlayground
